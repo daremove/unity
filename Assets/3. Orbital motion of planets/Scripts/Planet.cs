d3dcb252a8c6491ecbc24164c7140f4b0bd6f588
daremove <daremove1@gmail.com>
2021-02-22 21:54:04 +0500

    Added orbital motion of planets

---
+++ Assets/3. Orbital motion of planets/Scripts/Planet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    private const float G = 9.8f;
+    
+    public Transform attractionSource;
+    public Vector3 startSpeed = new Vector3(1, 1, 1);
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.velocity = startSpeed;
+        Debug.Log(_rigidbody);
+    }
+
+    private void Update()
+    {
+        Vector3 attraction = (attractionSource.position - transform.position).normalized;
+        
+        attraction = G * attraction / attraction.sqrMagnitude;
+        _rigidbody.AddForce(attraction);
+    }
+}
