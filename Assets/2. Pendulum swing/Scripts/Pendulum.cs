d5dc07da9fdd40235cb0d27c2c8e3f11a7ff54f0
daremove <daremove1@gmail.com>
2021-02-28 18:13:26 +0500

    Updated pendulum script

--- Assets/2. Pendulum swing/Scripts/Pendulum.cs
+++ Assets/2. Pendulum swing/Scripts/Pendulum.cs
@@ -6,34 +6,29 @@ public class Pendulum : MonoBehaviour
     
     private float l;
     private float offsetBall;
-    private float alpha;
     private float amplitude;
     private float cyclicFrequency;
-    
-    public float startPosition = 2;
-    public float startSpeed = 2;
     
     private void Start()
     {
         GameObject ceil = GameObject.FindGameObjectWithTag("PendulumCeil");
         GameObject ball = GameObject.FindGameObjectWithTag("PendulumBall");
 
-        startSpeed = startSpeed == 0 ? 1 : startSpeed;
-        startPosition = startPosition == 0 ? 1 : startPosition;
-        
         l = ceil.transform.position.y - (offsetBall = ball.transform.position.y);
-        
         cyclicFrequency = Mathf.Sqrt(G / l);
-        alpha = Mathf.Atan(cyclicFrequency * startPosition / startSpeed);
-        amplitude = startPosition / Mathf.Sin(alpha);
+        amplitude = Mathf.PI / 2f;
     }
 
     private void Update()
     {
+        float alpha = amplitude * Mathf.Cos(Time.realtimeSinceStartup * cyclicFrequency);
+        float x = l * Mathf.Sin(alpha);
+        float y = -l * Mathf.Cos(alpha) + offsetBall + l;
+        
         transform.position =
             new Vector3(
-                l * Mathf.Sin(amplitude * Mathf.Sin(Time.realtimeSinceStartup * cyclicFrequency + alpha)),
-                -l * Mathf.Cos(amplitude * Mathf.Sin(Time.realtimeSinceStartup * cyclicFrequency + alpha)) + l + offsetBall,
+                x,
+                y,
                 0f
             );
     }
